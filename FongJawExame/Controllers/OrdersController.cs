5046bbfa178bc58927461638114946072cb4e8cc
Jack Li <wa0327@gmail.com>
2025-02-05 04:17:23 +0800

    新增 orders

---
+++ FongJawExame/Controllers/OrdersController.cs
@@ -0,0 +1,45 @@
+﻿using FongJawExame.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace FongJawExame.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var token = Request.Cookies["JwtToken"];
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = (JwtSecurityToken)handler.ReadToken(token);
+            var userIdClaim = jsonToken.Claims.FirstOrDefault(claim => claim.Type == JwtRegisteredClaimNames.Sub);
+            if (userIdClaim == null)
+            {
+                return Unauthorized();
+            }
+
+            var userId = int.Parse(userIdClaim.Value);
+            var lastDate = DateTime.Today.AddDays(-30);
+
+            var query = from o in _context.Orders
+                        where o.UserId == userId
+                           && o.CreatedAt >= lastDate
+                           && o.TotalAmount > 5000
+                        orderby o.CreatedAt descending
+                        select o;
+
+            return View();
+        }
+    }
+}
---
+++ FongJawExame/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FongJawExame.Models
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
